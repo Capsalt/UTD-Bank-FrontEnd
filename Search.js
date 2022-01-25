428fdbdcdaabb5c2d4ba6eabebf7610c36727cee
Hasan Basri Aygümüş <hbasri2304@gmail.com>
2022-01-25 22:53:49 +0300

    Update and rename src to Search.js

---
+++ Search.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { Placeholder, Container } from 'react-bootstrap';
+
+const Search = () => {
+    <Container>
+    <p aria-hidden="true">
+      <Placeholder xs={6} />
+    </p>
+  
+    <Placeholder.Button xs={4} aria-hidden="true" />
+    </Container>
+};
+
+
+
+export default Search;
